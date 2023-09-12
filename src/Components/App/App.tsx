a5be3874a34669814a84858c6fd3e909d2650569
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-09-12 14:53:42 +0400

    update

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -25,7 +25,7 @@ const Item = ({ fileName, minimized, children }: Item) => {
   }
   return (
     <li>
-      {style === "folder" ? (visible ? "+" : "-") : ""}
+      {style === "folder" ? (visible ? ">" : '-') : ""}
       <span
         className={style}
         onClick={() => {
