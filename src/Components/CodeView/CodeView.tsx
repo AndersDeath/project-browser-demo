3488e80c368d7e32e4bbe632fe51b6f13dec740b
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-09-19 12:48:10 +0400

    interface update

--- src/Components/CodeView/CodeView.tsx
+++ src/Components/CodeView/CodeView.tsx
@@ -3,7 +3,7 @@ import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export const CodeView = ({ language, children }: any) => {
   return (
-    <SyntaxHighlighter showLineNumbers language={language} style={oneDark}>
+    <SyntaxHighlighter language={language} style={oneDark}>
       {children}
     </SyntaxHighlighter>
   );
--- src/Data/files.ts
+++ src/Data/files.ts
@@ -17,25 +17,25 @@ export const files = [
   {
     title: "index.tsx",
     id: "9",
-    content: `        import React from 'react';
-        import ReactDOM from 'react-dom/client';
-        import './index.scss';
-        import App from './Components/App/App';
-        import reportWebVitals from './reportWebVitals';
-        
-        const root = ReactDOM.createRoot(
-          document.getElementById('root') as HTMLElement
-        );
-        root.render(
-          <React.StrictMode>
-            <App />
-          </React.StrictMode>
-        );
-        
-        // If you want to start measuring performance in your app, pass a function
-        // to log results (for example: reportWebVitals(console.log))
-        // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-        reportWebVitals();
+    content: `import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './Components/App/App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
     `,
   },
   {},
