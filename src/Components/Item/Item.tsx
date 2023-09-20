8b0bc514282c5ca955be486558a05bf27e47b277
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-09-20 13:54:41 +0400

    update structure

---
+++ src/Components/Item/Item.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import './TreeView.scss';
+import { useFileContext } from '../../FileContext';
+import { extToString } from '../../Utils/ext';
+import { IItem } from '../../Data/interfaces';
+import { FilesData, files } from '../../Data/files';
+
+const Files = new FilesData(files);
+
+export const Item = ({ id, fileName, minimized, children }: IItem) => {
+  const [visible, setVisible] = useState(!minimized);
+  const { setFile } = useFileContext();
+  const style = `item ${extToString(fileName)}`;
+
+  return (
+    <li>
+      {style.indexOf('folder') !== -1 ? (
+        visible ? (
+          <span className='folder-arrow down'>{'>'}</span>
+        ) : (
+          <span className='folder-arrow'>{'>'}</span>
+        )
+      ) : (
+        ''
+      )}
+      <span
+        className={style}
+        onClick={() => {
+          const file = Files.get(id);
+          if (file) {
+            setFile(file);
+          }
+          if (children) {
+            setVisible(!visible);
+          }
+        }}
+      >
+        {fileName}
+      </span>
+      {children && visible && (
+        <ul>
+          {children.map((el: IItem) => {
+            return <Item key={el.id} {...el} />;
+          })}
+        </ul>
+      )}
+    </li>
+  );
+};
--- src/Components/TreeView/TreeView.tsx
+++ src/Components/TreeView/TreeView.tsx
@@ -1,52 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './TreeView.scss';
 import { FilesData, files } from '../../Data/files';
 import { useFileContext } from '../../FileContext';
-import { extToString } from '../../Utils/ext';
-import { IItem } from '../../Data/interfaces';
+import { Item } from '../Item/Item';
 
 const Files = new FilesData(files);
-
-const Item = ({ id, fileName, minimized, children }: IItem) => {
-  const [visible, setVisible] = useState(!minimized);
-  const { setFile } = useFileContext();
-  const style = `item ${extToString(fileName)}`;
-
-  return (
-    <li>
-      {style.indexOf('folder') !== -1 ? (
-        visible ? (
-          <span className='folder-arrow down'>{'>'}</span>
-        ) : (
-          <span className='folder-arrow'>{'>'}</span>
-        )
-      ) : (
-        ''
-      )}
-      <span
-        className={style}
-        onClick={() => {
-          const file = Files.get(id);
-          if (file) {
-            setFile(file);
-          }
-          if (children) {
-            setVisible(!visible);
-          }
-        }}
-      >
-        {fileName}
-      </span>
-      {children && visible && (
-        <ul>
-          {children.map((el: IItem) => {
-            return <Item key={el.id} {...el} />;
-          })}
-        </ul>
-      )}
-    </li>
-  );
-};
 
 export default function TreeView() {
   const { setFile } = useFileContext();
